1a164d15327c24eab8a3809b87f145df880b37fa
AikeNyanLynnOo <aikenyanlynnoo2399@gmail.com>
2021-07-31 23:28:36 +0630

    static serve added

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("./db.json");
-
+const middleWare = jsonServer.defaults({
+  static: "./public",
+});
+server.use(middleWare);
 const PORT = process.env.PORT || 8000;
 
 /* server.use(jsonServer.rewriter({
     "/api/*" : "/$1"
 }));
 */
-server.use("/api",router);
-server.listen(PORT,()=>{
-    console.log("Server is running");
-})
+server.use("/api", router);
+server.listen(PORT, () => {
+  console.log("Server is running");
+});
