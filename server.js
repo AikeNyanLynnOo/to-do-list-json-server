1e0b0b4c8dce4bfe6fd9af90d52278e268042dd6
AikeNyanLynnOo <aikenyanlynnoo2399@gmail.com>
2021-07-31 23:23:46 +0630

    static serve removed

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("./db.json");
-const middleWares = jsonServer.defaults({
-    static : "./public"
-});
+
 const PORT = process.env.PORT || 8000;
-server.use(middleWares);
+
 /* server.use(jsonServer.rewriter({
     "/api/*" : "/$1"
 }));
