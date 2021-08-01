60a1e1cdba0b9b779c4aee181724ade8656f77f8
AikeNyanLynnOo <aikenyanlynnoo2399@gmail.com>
2021-08-01 10:43:52 +0630

    Port change

--- server.js
+++ server.js
@@ -4,14 +4,11 @@ const router = jsonServer.router("./db.json");
 const middleWare = jsonServer.defaults({
   static: "./public",
 });
-server.use(middleWare);
-const PORT = process.env.PORT || 8000;
 
-/* server.use(jsonServer.rewriter({
-    "/api/*" : "/$1"
-}));
-*/
+server.use(middleWare);
+const PORT = process.env.PORT || 8001;
+
 server.use("/api", router);
 server.listen(PORT, () => {
-  console.log("Server is running");
+  console.log(`Server is running at port ${PORT}`);
 });
